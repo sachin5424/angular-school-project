3ebc7f67a460f9e9bf53d48951504435a6fcd1c0
sachin5424 <sachinsagar516@gmail.com>
2021-08-06 04:13:29 +0530

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static+'dist/school');
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/school'));
 
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/school/'}),
+);
 
-app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/school/index.html'))
-});
-
-
-app.listen(8080);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
